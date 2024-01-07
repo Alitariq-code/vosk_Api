97ac95f601f37870efb0a9c779084167c8a6ea57
Ali_Tariq <82971663+Alitariq-code@users.noreply.github.com>
2024-01-07 23:33:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,82 +1,80 @@
 from vosk import Model, KaldiRecognizer
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import requests
-import wave
-import json
 from pydub import AudioSegment
-from fun import analyze_audio, compare_lines, count_duplicate_lines, count_skipped_lines, count_words,calculate_word_count_ratio,get_wav_duration
+import time
+from fun import (
+    analyze_audio,
+    compare_lines,
+    count_duplicate_lines,
+    count_skipped_lines,
+    count_words,
+    calculate_word_count_ratio,
+    get_wav_duration,
+)
+from test import transcribe_audio_file
+import requests
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 
-model = Model("/home/alicode/Downloads/vosk-model-small-nl-0.22")
+def calculate_words_per_minute(words_correct, transcribed_text):
+    audio_duration = get_wav_duration('temp.wav')
+    
+    # Check if audio_duration is greater than zero before division
+    if audio_duration > 0:
+        words_per_minute = (words_correct / audio_duration) * 60
+    else:
+        # Set a default value or handle it as per your requirements
+        words_per_minute = None
+
+    return words_per_minute
 
 
-
-def calculate_error_metrics(original_text, transcribed_text):
-    """
-    Calculate error metrics between original and transcribed text.
-
-    Parameters:
-    - original_text (str): Original text.
-    - transcribed_text (str): Transcribed text.
-
-    Returns:
-    - dict: Error metrics.
-    """
+def calculate_error_metrics(original_text, transcribed_text, delete, insert, sub, manual_text):
     words_original = original_text.split()
     words_transcribed = transcribed_text.split()
 
-    wc = len(set(words_original) & set(words_transcribed))  # Words Correct
-    wr = len(words_transcribed)  # Words Reads
+    if manual_text:
+        words_manual_text = manual_text.split()
+        wt_maual = len(words_manual_text)
+    else:
+        words_manual_text = []
+        wt_maual = 0
 
-    # Implement your logic to calculate other error metrics
-    # ...
+    wr = len(words_transcribed)
+    wt_original = len(words_original)
+    wc = wt_original - (delete + insert + sub)
+
+    acc = wc * 100 / wt_original
+    wc = max(0, wc)
+    acc = max(0, acc)
+    oriVsTran = 100 * min(wt_original / wr, wr / wt_original)
+    manualVsTrans = 100 * min(wt_maual / wr, wr / wt_maual) if wt_maual > 0 else 0
+    manualVsorginal = 100 * min(wt_maual / wt_original, wt_original / wt_maual) if wt_maual > 0 else 0
 
     error_metrics = {
         'WR': wr,
         'WC': wc,
         'Words Correct per Minute': calculate_words_per_minute(wc, transcribed_text),
-        # Add other error metrics as needed
+        'Acc': acc,
+        'oriVsTran': oriVsTran,
+        'manualVsTrans': manualVsTrans,
+        'manualVsorginal': manualVsorginal
     }
 
     return error_metrics
 
-def calculate_words_per_minute(words_correct, transcribed_text):
-    """
-    Calculate words correct per minute.
+def calculate_word_count_ratio(transcribed_text, original_text, max_ratio=100):
+    word_count_org = count_words(original_text)
+    word_count_transcribed = count_words(transcribed_text)
 
-    Parameters:
-    - words_correct (int): Number of words correct.
-    - transcribed_text (str): Transcribed text.
-
-    Returns:
-    - float: Words correct per minute.
-    """
-    # Implement your logic to calculate words correct per minute
-    # ...
-
-    # Placeholder value, replace with actual calculation
-    audio_duration = 120  # seconds
-    words_per_minute = (words_correct / audio_duration) * 60
-
-    return words_per_minute
+    ratio = min(100*word_count_org / word_count_transcribed , word_count_transcribed* 100/word_count_org)
+    return ratio
 
 def calculate_pause_metrics(transcribed_text):
-    """
-    Calculate pause metrics in the transcribed text.
-
-    Parameters:
-    - transcribed_text (str): Transcribed text.
-
-    Returns:
-    - dict: Pause metrics.
-    """
-    # Implement your logic to calculate pause metrics
-    # ...
-
-    # Placeholder values, replace with actual calculations
+    # Your logic for pause metrics calculation goes here
+    # Placeholder values are used, replace them with actual calculations
     pauses_1_3_seconds = 5
     hesitations_3_seconds = 2
 
@@ -87,93 +85,197 @@ def calculate_pause_metrics(transcribed_text):
     }
 
     return pause_metrics
-def transcribe_audio_vosk(audio_file_path):
-    wf = wave.open(audio_file_path, "rb")
-    recognizer = KaldiRecognizer(model, wf.getframerate())
-    
-    accumulated_text = ""
-    
-    while True:
-        data = wf.readframes(4000)
-        if len(data) == 0:
-            break
-        recognizer.AcceptWaveform(data)
-    
-    result = json.loads(recognizer.FinalResult())
-    accumulated_text += result.get("text", "")
-    
-    return accumulated_text
 
+def format_word_list(word_list):
+    return [{'ID': entry['ID'], 'Word': entry['Word']} for entry in word_list]
+bufferData=[]
 @app.route('/process_audio', methods=['POST'])
 def process_audio():
     try:
         data = request.get_json()
         audio_url = data.get('audio_url')
         original_text = data.get('original_text')
-
+       
+        manual_text= data.get('manual_text')
+        transcrib = ''
+        id = data.get('id')
         # Download the audio file from the provided URL
-        response = requests.get(audio_url)
-        if response.status_code != 200:
-            raise Exception(f"Failed to download audio from the provided URL. Status code: {response.status_code}")
+        if id and audio_url:
+            response = requests.get(audio_url)
+            if response.status_code != 200:
+                raise Exception(f"Failed to download audio from the provided URL. Status code: {response.status_code}")
 
-        # Save audio data to a temporary file
-        audio_file_path = "temp_original.wav"
-        with open(audio_file_path, "wb") as temp_file:
-            temp_file.write(response.content)
+            # Save audio data to a temporary file
+            audio_file_path = "temp_original.wav"
+            with open(audio_file_path, "wb") as temp_file:
+                temp_file.write(response.content)
 
-        # Convert audio to mono and set sample width to 2 bytes
-        audio = AudioSegment.from_file(audio_file_path)
-        audio = audio.set_channels(1).set_sample_width(2)
-        audio.export("temp.wav", format="wav")
+            # Convert audio to mono and set sample width to 2 bytes
+            audio = AudioSegment.from_file(audio_file_path)
+            audio = audio.set_channels(1).set_sample_width(2)
+            audio.export("temp.wav", format="wav")
 
-        # Transcribe audio using Vosk
-        transcribed_text = transcribe_audio_vosk("temp.wav")
-        deleted_words, inserted_words, substituted_words, repeated_words = compare_lines(original_text, transcribed_text)
-        duplicate_lines = count_duplicate_lines(transcribed_text)
-        skipped_lines = count_skipped_lines(transcribed_text)
-        word_count = count_words(transcribed_text)
-
-
-    # Calculate error metrics
-        error_metrics = calculate_error_metrics(original_text, transcribed_text)
-
-        # Calculate pause metrics
-        pause_metrics = calculate_pause_metrics(transcribed_text)
-        # Additional analysis and metrics calculations go here...
-
-        # Calculate accuracy, audio duration, and transcription confidence score
-        accuracy = error_metrics.get('Words Correct per Minute', 0) / word_count * 100 if word_count != 0 else 0
-        audio_duration = get_wav_duration('temp.wav')
-        # transcription_confidence = confidence if confidence is not None else 0
-
-        original_vs_audio = calculate_word_count_ratio(transcribed_text, original_text)
-        analysis_result = analyze_audio('temp.wav')
-        # Prepare JSON response with additional outcomes
-        response = {
-            'transcribed_text': transcribed_text,
-            # 'confidence': confidence,  # Confidence score
-            'analysis_result': analysis_result,
-            'deleted_words': deleted_words,
-            'inserted_words': inserted_words,
-            'substituted_words': substituted_words,
-            'repeated_words': repeated_words,
-            'duplicate_lines': duplicate_lines,
-            'skipped_lines': skipped_lines,
-            'word_count': word_count,
-            'error_metrics': error_metrics,
-            'pause_metrics': pause_metrics,
-            'repeated_words_length': len(repeated_words),
-            'original_vs_audio': original_vs_audio,
-            'accuracy': accuracy,
-            'audio_duration': audio_duration,
-            'transcription_confidence': 76,
-            # Add other metrics as needed
+            # Transcribe audio using Vosk
+            transcribed_text = transcribe_audio_file("temp.wav")
+            newData = {}
+            newData["id"] = id
+            newData["Text"] = transcribed_text
+            bufferData.append(newData)
+            print(bufferData)
+            response = {
+            'staus': 'done with audio at vosk Api'
         }
+            return jsonify(response)
 
-        return jsonify(response)
+        else:
+            print("okok")
+            print("Searching for ID:", id)
+
+            start_time = time.time()
+            timeout_duration = 90  # 90 seconds = 1.5 minutes
+            id_found = False
+
+            while not id_found and time.time() - start_time < timeout_duration:
+                elapsed_time = time.time() - start_time
+                print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+                for item in bufferData:
+                    if 'id' in item and item['id'] == id:
+                        transcrib = item['Text']
+                        print("Data of this:", transcrib)
+                        id_found = True  # Set the flag to True when ID is found
+                        break      
+        # print(transcribed_text)
+            substituted_words,delete_words,insert_words,merged= compare_lines(original_text, transcrib)
+            
+            # error_metrics = calculate_error_metrics(original_text, transcribed_text)
+
+            # Calculate pause metrics
+            pause_metrics = calculate_pause_metrics(transcrib)
+
+            # Convert DataFrames to lists of dictionaries
+            delete, insert, sub = len(delete_words), len(insert_words), len(substituted_words)
+            print(delete,insert,sub)
+            error_metrics = calculate_error_metrics(original_text, transcrib, delete, insert, sub, manual_text)
+
+            formatted_deleted_words = format_word_list(delete_words)
+            formatted_inserted_words = format_word_list(insert_words)
+            formatted_substituted_words = format_word_list(substituted_words)
+            merged_format = format_word_list(merged)
+
+
+        
+            accuracy=error_metrics['Acc']
+
+            original_vs_audio = calculate_word_count_ratio(transcrib, original_text)
+
+            transcription_confidence = 76  
+
+            audio_duration = get_wav_duration('temp.wav')
+
+            # original_vs_audio = calculate_word_count_ratio(transcribed_text, original_text)
+            analysis_result = analyze_audio('temp.wav')
+            # Prepare JSON response with additional outcomes
+            response = {
+        'transcribed_text': transcrib,
+        'analysis_result': analysis_result,
+        'deleted_words': formatted_deleted_words,
+        'inserted_words': formatted_inserted_words,
+        'merged':merged,
+        'substituted_words': formatted_substituted_words,
+        'duplicate_lines': count_duplicate_lines(transcrib),
+        'skipped_lines': count_skipped_lines(transcrib),
+        # 'word_count': word_count,
+        'error_metrics': error_metrics,
+        'pause_metrics': pause_metrics,
+        'original_vs_audio': error_metrics['oriVsTran'],
+        'manualVsTrans':error_metrics['manualVsTrans'],
+        'manualVsorginal':error_metrics['manualVsorginal'],
+        'accuracy': accuracy,
+        'audio_duration': audio_duration,
+        'transcription_confidence': transcription_confidence,
+        # Add other metrics as needed
+    }
+            return jsonify(response)
 
     except Exception as e:
         return jsonify({'error': str(e)})
+# @app.route('/process_audio', methods=['POST'])
+# def process_audio():
+#     try:
+#         data = request.get_json()
+#         audio_url = data.get('audio_url')
+#         original_text = data.get('original_text')
+
+#         # Download the audio file from the provided URL
+#         response = requests.get(audio_url)
+#         if response.status_code != 200:
+#             raise Exception(f"Failed to download audio from the provided URL. Status code: {response.status_code}")
+
+#         # Save audio data to a temporary file
+#         audio_file_path = "temp_original.wav"
+#         with open(audio_file_path, "wb") as temp_file:
+#             temp_file.write(response.content)
+
+#         # Convert audio to mono and set sample width to 2 bytes
+#         audio = AudioSegment.from_file(audio_file_path)
+#         audio = audio.set_channels(1).set_sample_width(2)
+#         audio.export("temp.wav", format="wav")
+
+#         # Transcribe audio using Vosk
+#         transcribed_text = transcribe_audio_file("temp.wav")
+#         # print(transcribed_text)
+#         substituted_words,delete_words,insert_words= compare_lines(original_text, transcribed_text)
+        
+#         # error_metrics = calculate_error_metrics(original_text, transcribed_text)
+
+#         # Calculate pause metrics
+#         pause_metrics = calculate_pause_metrics(transcribed_text)
+
+#         # Convert DataFrames to lists of dictionaries
+#         delete, insert, sub = len(delete_words), len(insert_words), len(substituted_words)
+#         print(delete,insert,sub)
+#         error_metrics = calculate_error_metrics(original_text, transcribed_text, delete=delete, insert=insert, sub=sub)
+
+#         formatted_deleted_words = format_word_list(delete_words)
+#         formatted_inserted_words = format_word_list(insert_words)
+#         formatted_substituted_words = format_word_list(substituted_words)
+
+
+       
+#         accuracy=error_metrics['Acc']
+
+#         original_vs_audio = calculate_word_count_ratio(transcribed_text, original_text)
+
+#         transcription_confidence = 76  
+
+#         audio_duration = get_wav_duration('temp.wav')
+
+#         # original_vs_audio = calculate_word_count_ratio(transcribed_text, original_text)
+#         analysis_result = analyze_audio('temp.wav')
+#         # Prepare JSON response with additional outcomes
+#         response = {
+#     'transcribed_text': transcribed_text,
+#     'analysis_result': analysis_result,
+#     'deleted_words': formatted_deleted_words,
+#     'inserted_words': formatted_inserted_words,
+#     'substituted_words': formatted_substituted_words,
+#     'duplicate_lines': count_duplicate_lines(transcribed_text),
+#     'skipped_lines': count_skipped_lines(transcribed_text),
+#     # 'word_count': word_count,
+#     'error_metrics': error_metrics,
+#     'pause_metrics': pause_metrics,
+#     'original_vs_audio': original_vs_audio,
+#     'accuracy': accuracy,
+#     'audio_duration': audio_duration,
+#     'transcription_confidence': transcription_confidence,
+#     # Add other metrics as needed
+# }
+#         return jsonify(response)
+
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
